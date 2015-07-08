3560c0f763baf709aa957eac7119a72d28e12c85
Kin Lane <info@apievangelist.com>
2015-07-07 22:40:38 -0700

    add to two more levels

--- api/methods/retail.php
+++ api/methods/retail.php
@@ -9,6 +9,7 @@ require_once('m-curated-byweek-archive-get.php');
 
 require_once('m-curated-tags-get.php');
 
+require_once('m-curated-tags-tag-get.php');
 require_once('m-curated-tags-tag-build-get.php');
 require_once('m-curated-tags-byweek-get.php');
 
